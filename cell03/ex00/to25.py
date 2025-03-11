1ea6cee3bd9ba15fd121c883aa40fe3f4ce697c5
Phanthipha127 <42910@prot.ac.th>
2025-03-11 14:10:40 +0700

    Delete cell03/ex00/to25.py

--- cell03/ex00/to25.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Enter a number less than 25: "))
-if num > 25:
-    print("Error")
-else:
-    while num <= 25:
-        print(f"Inside the ioop, my variable is {num}")
-        num += 1
