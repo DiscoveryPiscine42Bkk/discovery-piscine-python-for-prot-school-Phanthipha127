8d73823cff1e485bdc6a92a44e76a2e773855fa6
Phanthipha127 <42910@prot.ac.th>
2025-03-11 15:30:11 +0700

    Delete cell03/ex03/advanced_mult.py

--- cell03/ex03/advanced_mult.py
+++
@@ -1,5 +0,0 @@
-for num in range(1, 11):
-    print(f"Multiplication table for {num}:")
-    for multiplier in range(1, 11):
-        print(f"{num} x {multiplier} = {num * multiplier}")
-        print("")
