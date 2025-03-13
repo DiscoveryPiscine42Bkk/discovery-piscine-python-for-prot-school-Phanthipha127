bbebdfa0add1c18aa730988189d5d8518ec90802
Phanthipha127 <42910@prot.ac.th>
2025-03-13 09:05:21 +0700

    Delete cell06/ex03/greetings_for_all.py

--- cell06/ex03/greetings_for_all.py
+++
@@ -1,8 +0,0 @@
-def greetings(name="noble stranger"):
-    if not isinstance(name, str):
-        print("Error: Name must be a string.")
-    else:
-        print(f"Welcome, {name}!")
-greetings()
-greetings("John")
-greetings(123)
